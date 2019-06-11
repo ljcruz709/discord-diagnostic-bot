cb4aeca5ef4d9e1797de0737d9109b40a27d5d0f
EsterUscanga <ester.uscanga@cceo.com.mx>
2019-06-10 22:00:14 -0500

    Was reformated the code

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const sets = JSON.parse(Fs.readFileSync('set.json')); //set.json stores the sets
 
 
 client.on(`ready`, () => {
-    console.log(`bot is on :)`);
+	console.log(`bot is on :)`);
 });
 
 client.on(`message`, mensaje => {
-    console.log(`${mensaje.author.tag} dice : ${mensaje.content}`);
-    
-//    if (mensaje.author.tag === "TeyNavi#1245"){
-//       mensaje.channel.send(`${mensaje.author}, hoy estás radiante :)`)
-//    }else{
-        if(mensaje.content === "comenzar"){
-            let i =  0
-            let user = mensaje.author
-            mensaje.channel.send(`${sets[0]["preguntas"][i]["pregunta"]}\n${sets[0]["preguntas"][i]["respuestas"]}`)    
-            
-        }
-//    }
+	console.log(`${mensaje.author.tag} dice : ${mensaje.content}`);
+
+	//    if (mensaje.author.tag === "TeyNavi#1245"){
+	//       mensaje.channel.send(`${mensaje.author}, hoy estás radiante :)`)
+	//    }else{
+	if (mensaje.content === "comenzar") {
+		let i = 0
+		let user = mensaje.author
+		mensaje.channel.send(`${sets[0]["preguntas"][i]["pregunta"]}\n${sets[0]["preguntas"][i]["respuestas"]}`)
+
+	}
+	//    }
 });
 
 //add bot's token here. Remember to update everyday
