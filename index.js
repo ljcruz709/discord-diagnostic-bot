ff0e0935178f02be2575350df807b3ef5898e6a0
ljcruz709 <ljcruz709@gmail.com>
2019-06-13 20:11:39 -0500

    JSON for test conclussions and for saving test results. Added funtions to send datased by chat and abort test at any moment.

--- index.js
+++ index.js
@@ -19,11 +19,34 @@ client.on(`message`, mensaje => {
             if (preguntas[testers[j].count]["pesos"][mensaje.content] != null) {
                 testers[j].count++
                 testers[j].respuestas.push(mensaje.content)
-                
+                              
+
+
                 if(testers[j].count < preguntas.length){               
                     mensaje.channel.send(`${mensaje.author}, ${preguntas[testers[j].count]["pregunta"]}\n${preguntas[testers[j].count]["respuestas"]}`)             
                 }else{
-                    mensaje.channel.send(`${mensaje.author}, Tus respuestas fueron ${testers[j].respuestas}`)
+                    let naturalezas = JSON.parse(Fs.readFileSync('naturalezas.json'));
+                    for (let i = 0; i < testers[j].respuestas.length; i++) {
+                        naturalezas["count"][preguntas[i]["caracteristicas"][testers[j].respuestas[i]]] += [preguntas[i]["pesos"][testers[j].respuestas[i]]]                        
+                    }
+                    let max = 0
+                    let ind
+                    for (let i = 0; i < naturalezas["nombres"].length; i++) {
+                        if (naturalezas["count"][naturalezas["nombres"][i]] > max){
+                            max = naturalezas["count"][naturalezas["nombres"][i]]
+                            ind = i
+                        }
+                    }
+                    mensaje.channel.send(`${mensaje.author}, Tu perfil es ${naturalezas["nombres"][ind]}`)
+                    mensaje.channel.send(`${naturalezas["descripciones"][naturalezas["nombres"][ind]]}`)
+
+                    let ds = JSON.parse(Fs.readFileSync('dataset.json'));
+                    testers[j].respuestas.push(naturalezas["nombres"][ind])
+                    ds.push(testers[j].respuestas)
+                    Fs.writeFile('dataset.json', JSON.stringify(ds), function(err){
+                        if(err) throw err
+                        console.log("Registrado")
+                    })
                     testers.splice(j, 1)
                 }
             }else{
@@ -38,15 +61,25 @@ client.on(`message`, mensaje => {
         }
     
     }
-       
-        if(mensaje.content === "comenzar" && found){
-            let temp = {}            
-            temp.user = mensaje.author
-            temp.count = 0
-            temp.respuestas = []
-            testers.push(temp)
-            mensaje.channel.send(`${mensaje.author}, ${preguntas[0]["pregunta"]}\n${preguntas[0]["respuestas"]}`)             
-            
+        if (found) {
+            if(mensaje.content === "comenzar"){
+                let temp = {}            
+                temp.user = mensaje.author
+                temp.count = 0
+                temp.respuestas = []
+                testers.push(temp)
+                mensaje.channel.send(`${mensaje.author}, ${preguntas[0]["pregunta"]}\n${preguntas[0]["respuestas"]}`)             
+            }
+            if (mensaje.content === "dataset"){
+                mensaje.channel.send({
+                    files: [{
+                      attachment: '',
+                      name: 'dataset.json'
+                    }]
+                  })
+                    .then(console.log("sent"))
+                    .catch(console.error("dataset not found"));
+            }
         }
 });
 
